e7ea75274d51bda72f6abe069fc5d259df5fded5
Gustavo Cassel <guscassel@gmail.com>
2021-05-30 21:50:28 -0300

    Improved UI adding a Play button

--- ui/js/MusicFunctions.js
+++ ui/js/MusicFunctions.js
@@ -1,8 +1,8 @@
 
 const MUSIC_URL = 'http://192.168.10.247:8080/function/crud/';
 
-function LoadMusicData() {
-    const result = Get(MUSIC_URL);
+function loadMusicData() {
+    const result = get(MUSIC_URL);
     if (result) {
         var parsed_result = JSON.parse(result);
         return parsed_result;
@@ -11,9 +11,28 @@ function LoadMusicData() {
 };
 
 
-function Get(targetUrl) {
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET", targetUrl, false);
-    Httpreq.send();
-    return Httpreq.responseText;
+function get(targetUrl) {
+    var request = new XMLHttpRequest();
+    request.open("GET", targetUrl, false);
+    request.send();
+    return request.responseText;
+}
+
+function playMusic(id) {
+    var video = document.getElementById('video');
+    var videoSrc = 'http://192.168.10.247:8080/function/streaming/' + id + '/outputlist.m3u8';
+    //
+    // First check for native browser HLS support
+    //
+    if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+        //
+        // If no native HLS support, check if HLS.js is supported
+        //
+    } else if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+    }
+    video.play()
 }
