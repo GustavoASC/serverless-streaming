6e9b10d9f59a93b8d87e2e8d20132c2be53a4169
Gustavo Cassel <guscassel@gmail.com>
2021-06-02 22:21:17 -0300

    Refactors on chunk persistence

--- functions/crud/crud/handler.py
+++ functions/crud/crud/handler.py
@@ -91,20 +91,25 @@ class ChunksPersistence:
     def __init__(self, db):
         self.chunks_col = db.get_chunks_collection()
 
-    def _split_music_chunks(self, song_base64):
+    def _split_chunks(self, song_base64):
         song_bytes = song_base64.encode("ascii")
         song_bytes = base64.b64decode(song_bytes)
         return MusicSplitter().split(song_bytes)
 
+    def _insert_chunk(self, music_id, name, bytes):
+        data = {
+            "music_id": str(music_id),
+            "chunk_name": name,
+            "chunk_bytes": bytes
+        }
+        self.chunks_col.insert_one(data)
+
     def insert_song_chunks(self, music_id, song_base64):
-        chunks = self._split_music_chunks(song_base64)
+        chunks = self._split_chunks(song_base64)
         for key in chunks:
-            chunk = {
-                "music_id": str(music_id),
-                "chunk_name": key,
-                "chunk_bytes": chunks[key]
-            }
-            self.chunks_col.insert_one(chunk)
+            self._insert_chunk(music_id,
+                               key,
+                               chunks[key])
 
     def delete_song_chunks(self, music_id):
         self.chunks_col.delete_many({"music_id": music_id})
